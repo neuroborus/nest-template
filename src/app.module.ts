aa7739322c15174269ee691c3cf9ae1a95397fa1
Michael <mihasso@protonmail.com>
2025-10-28 12:21:37 +0100

    chore(pino): add pino pretty and censor

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { LoggerModule } from 'nestjs-pino';
 import { ConfigModule } from '@/config';
 import { HealthApi } from '@/apis/health';
+import { V1Api } from '@/apis/v1';
 import { pinoHttp } from './pino';
 
 @Module({
